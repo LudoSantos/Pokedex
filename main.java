19f821bf4592b5315312db039c9c53da379a5cfb
Senpai-kun <luisfernandooliveira0704@gmail.com>
2024-12-07 21:26:19 -0300

    Update main.java

--- main.java
+++ main.java
@@ -7,8 +7,20 @@ import Colection.Pokemon;
 
 public class main {
     public static void main(String[] args) {
-        Estatisticas estatisticas = new Estatisticas(20, 30, 40, 20, 10, 100, "Pokemon veloz que adora voar pelos áres de alola.");
-        Pokemon pokemon1 = new Pokemon("Passaro", 20, -2, 40, estatisticas);
-        System.out.println(pokemon1.toString()); 
-       }
+
+        Estatisticas estatisticas = new Estatisticas(20, 30, -40, 20, 10, 100,
+                "Pokemon veloz que adora voar pelos áres de alola.");
+
+        Pokemon pokemon1 = new Pokemon("Passaro", 20, 2, 40, estatisticas);
+        System.out.println(pokemon1.toString());
+
+        Pokemon evolucao = new Pokemon("Passaro Grande", 30);
+
+        pokemon1.adicionarEvoluçao1(evolucao);
+        System.out.println(pokemon1.toString());
+
+        Pokemon evolucao2 = new Pokemon("Passaro Muito Grande", 50);
+        pokemon1.adicionarEvoluçao2(evolucao2);
+        System.out.println(pokemon1.toString());
+    }
 }
