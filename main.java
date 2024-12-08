f647904f9c88a089d70a3ee7f709465f7ac4d41d
Senpai-kun <luisfernandooliveira0704@gmail.com>
2024-12-07 22:07:15 -0300

    Update main.java

--- main.java
+++ main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import Colection.Estatisticas;
 import Colection.Pokemon;
+import Colection.Tipos;
 
 public class main {
     public static void main(String[] args) {
@@ -11,7 +12,8 @@ public class main {
         Estatisticas estatisticas = new Estatisticas(20, 30, -40, 20, 10, 100,
                 "Pokemon veloz que adora voar pelos áres de alola.");
 
-        Pokemon pokemon1 = new Pokemon("Passaro", 20, 2, 40, estatisticas);
+        Tipos tipo = new Tipos("Pedra", "Voador");
+        Pokemon pokemon1 = new Pokemon("Passaro", 20, 2, 40, 0, estatisticas, tipo);
         System.out.println(pokemon1.toString());
 
         Pokemon evolucao = new Pokemon("Passaro Grande", 30);
